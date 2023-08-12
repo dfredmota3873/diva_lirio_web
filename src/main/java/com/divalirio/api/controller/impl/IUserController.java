d09d5db865fcfe6f426f8429421d17d34168dc09
Fred <dfredmota3873@gmail.com>
2023-08-12 00:48:02 -0300

    open api

---
+++ src/main/java/com/divalirio/api/controller/impl/IUserController.java
@@ -0,0 +1,39 @@
+package com.divalirio.api.controller.impl;
+
+import com.divalirio.api.dto.CreateUserDTORequest;
+import com.divalirio.api.dto.UserDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Tag(name = "Api de Usuários")
+public interface IUserController {
+
+    @Operation(summary = "Salva um novo Usuario", description = "Realiza o cadastro de um novo usuário")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Caso o usuário seja cadastrado com sucesso."
+            )
+    })
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@RequestBody CreateUserDTORequest dtoRequest);
+    @Operation(summary = "Lista todos os usuários")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Caso a consulta seja efetuada com sucesso."
+            )
+    })
+    @GetMapping()
+    ResponseEntity<List<UserDTO>> findAll();
+
+}
--- src/main/java/com/divalirio/api/controller/impl/UserController.java
+++ src/main/java/com/divalirio/api/controller/impl/UserController.java
@@ -1,4 +1,4 @@
-package com.divalirio.api;
+package com.divalirio.api.controller.impl;
 
 import com.divalirio.api.dto.CreateUserDTORequest;
 import com.divalirio.api.dto.UserDTO;
@@ -6,26 +6,34 @@ import com.divalirio.mapper.UserConverter;
 import com.divalirio.model.User;
 import com.divalirio.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
-public class UserController {
+public class UserController implements IUserController {
 
     private final UserService service;
 
     private final UserConverter converter;
 
-    @PostMapping(value = "/sign-up")
+    @PostMapping(value = "/create")
     public ResponseEntity<UserDTO> createUser(@RequestBody CreateUserDTORequest dtoRequest){
 
         User user = service.create(converter.convertDTOtoModel(dtoRequest));
 
         return ResponseEntity.ok(converter.convertModelToDTO(user));
+    }
+
+    @Override
+    public ResponseEntity<List<UserDTO>> findAll() {
+        return ResponseEntity.ok(converter.convertListModelToDTO(service.findAll()));
     }
 }
